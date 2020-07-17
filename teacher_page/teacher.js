688d78b73c4cc95f1e443d16b4bb280bd10ebc1f
Avishay141 <Avishay141@gmail.com>
2020-07-17 14:24:47 +0300

    added the teacher profile form

--- teacher_page/teacher.js
+++ teacher_page/teacher.js
@@ -1,15 +1,25 @@
 
 const  db = firebase.database();
 
+//profile attributes
+var languages_I_teach = [];
+var also_speak = [];
+var homeCountry ="";
 
-// firebase.auth().onAuthStateChanged(firebaseUser => {
-//   if(!firebaseUser)
-//      window.location = "../login/index.html";
-//
-//   else
-//     console.log("this line shoud be executed once for each login");
-//
-// })
+
+
+
+var userID;
+firebase.auth().onAuthStateChanged(function(user) {
+  if (!user) {
+
+     window.location = "../login/index.html";
+  } else {
+    userID = user.uid;
+    console.log("this line shoud be executed once for each login");
+  }
+});
+
 
 
 $("#logout_btn").on("click",function(){
@@ -17,9 +27,9 @@ $("#logout_btn").on("click",function(){
 });
 
 
-$("select.country").on("click", function(e) {
- var selectedCountry = $(this).children("option:selected").val();
-  console.log(selectedCountry)//get text of each li
+$("select.home_country").on("click", function(e) {
+  homeCountry = $(this).children("option:selected").val();
+  console.log("&&&&&&&&&&&&&77777: "  +homeCountry);
 
 });
 
@@ -32,8 +42,7 @@ $('input[name=checkbox]').change(function(){
 });
 
 
-var languages_I_teach = [];
-var also_speak = [];
+
 
 function upadate_array_by_checkbox(name, arr){
   var items=document.getElementsByName(name);
@@ -49,14 +58,19 @@ $("#save_btn").on("click",function(){
   upadate_array_by_checkbox('speak', also_speak);
   console.log("languages_I_teach: " + languages_I_teach);
   console.log("also_speak: " + also_speak);
+  path = "Users/Teachers/"+userID+"/";
 
-  // db.ref("Users/Students/WB4ZK1wiK3RGwhvd1hQ67gvENEz1").on("value", read);
-  //db.ref("Users/Students/WB4ZK1wiK3RGwhvd1hQ67gvENEz1/").update({'list':also_speak});
-  window.location.replace("../main/main.html");
+  db.ref(path).update({'name':$(".Name").val()});
+  db.ref(path).update({'homeCountry':homeCountry});
+  db.ref(path).update({'languages_I_teach':languages_I_teach});
+  db.ref(path).update({'alsoSpeak':also_speak});
+  db.ref(path).update({'lessonPrice':$(".Lesson_price").val()});
+
 
 });
 
 function read(data){
+    // db.ref("Users/Students/WB4ZK1wiK3RGwhvd1hQ67gvENEz1").on("value", read);
   console.log("enter got_data");
   var questions_obj = data.val();
   // var keys = Object.keys(questions_obj);
